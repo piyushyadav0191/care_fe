968eba30b9b0e420bb9c04947c7f23bbd9218f8e
Piyush Yadav <piyushyadav0191@gmail.com>
2024-12-29 10:50:53 +0530

    Update src/components/Users/UserListAndCard.tsx
    Co-authored-by: Jacob John Jeevan <40040905+Jacobjeevan@users.noreply.github.com>

--- src/components/Users/UserListAndCard.tsx
+++ src/components/Users/UserListAndCard.tsx
@@ -100,7 +100,7 @@ const getNameAndStatusCard = (
                   </h1>
                 </TooltipTrigger>
                 <TooltipContent
-                  className="px-4 py-3 text-sm bg-black text-white rounded-xl shadow-xl border border-indigo-300 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+                  className="px-4 py-3 text-sm bg-black text-white rounded-md shadow-lg"
                   sideOffset={5}
                 >
                   <p className="font-semibold">{formatName(user)}</p>
